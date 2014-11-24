77130d8c60773c577c498ca444d0da75370ad1b9
Pat Thomas <pthomas@relaynetwork.com>
2014-11-24 09:18:14 -0500

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rhombot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rhombot/core_test.clj
@@ -0,0 +1,7 @@
+(ns rhombot.core-test
+  (:require [clojure.test :refer :all]
+            [rhombot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
