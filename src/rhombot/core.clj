6aef3688437a58f06146dbb92abc99434d3a5bd7
Pat Thomas <pthomas@relaynetwork.com>
2014-11-25 09:21:19 -0500

    core.async dependency.

--- src/rhombot/core.clj
+++ src/rhombot/core.clj
@@ -1,4 +1,4 @@
 (ns rhombot.core
-  (:require [rhombot.sounds   :as sounds]
-            [rhombot.playback :as playback]))
-
+  (:require [rhombot.sounds     :as sounds]
+            [rhombot.playback   :as playback]
+            [clojure.core.async :as async]))
